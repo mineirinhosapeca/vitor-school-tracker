7f0d0533019f2423068d1db57d1267e734162379
VitorVitinho <vitor.barbosa.silva23@escola.pr.gov.br>
2026-09-18 08:49:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+fuction calcularMinutosFora(isoString){ 
+  const saída = new Date(isoString);
+  const agora = new date();
+  const diff = Math.floor((agora - saída) / 60000);
+  return diff;
+}
+
+function renderizarDashboard(movimentacoes) {
+  const grid = document.getElementById('grid-alunos');
+  grid.innerHTML = '';
+
+  movimentacoes.forEach(m => {
+    if (m.status === 'em_andamento') {
+      const minutos = calcularMinutosFora(m.data_hora_saída);
+      const isCritical = minutos > 10;
+
+      const card = 
